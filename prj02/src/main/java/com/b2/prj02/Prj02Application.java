9d31e54cb5afab82d07ac7b59df6e4a93d15a22e
jin1220 <h6779@naver.com>
2023-11-14 15:08:59 +0900

    first

---
+++ prj02/src/main/java/com/b2/prj02/Prj02Application.java
@@ -0,0 +1,13 @@
+package com.b2.prj02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj02Application.class, args);
+	}
+
+}
---
+++ prj02/src/test/java/com/b2/prj02/Prj02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2.prj02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
