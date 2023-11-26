149e29a64bb1fecba45e80d15b3e5c8048ee6b62
jin1220 <h6779@naver.com>
2023-11-26 13:14:22 +0900

    유저 장바구니 조회

--- prj02/src/main/java/com/b2/prj02/controller/ProfileController.java
+++ prj02/src/main/java/com/b2/prj02/controller/ProfileController.java
@@ -4,6 +4,8 @@ package com.b2.prj02.controller;
 import com.b2.prj02.dto.response.ProfileInfoResponseDTO;
 
 
+import com.b2.prj02.dto.response.ShoppingCartResponseDTO;
+import com.b2.prj02.service.CartService;
 import com.b2.prj02.service.ProfileService;
 import com.b2.prj02.service.jwt.TokenContext;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProfileController {
 
     private final ProfileService profileService;
+    private final CartService cartService;
     // 유저 정보 조회
     @GetMapping("/info")
     public ResponseEntity<?> getUserProfile() {
@@ -28,13 +33,14 @@ public class ProfileController {
         ProfileInfoResponseDTO profileInfoResponse = profileService.findProfileInfoByProfileEmail(profileId);
         return ResponseEntity.ok(profileInfoResponse);
     }
-    // 장바구니 조회
-    // TODO : 미완성
-//    @GetMapping("/cart")
-//    public ShoppingCartListResponseDTO getShoppingCart(){
-//        String profileEmail = TokenContext.getProfileEmail();
-//        return profileService.getShoppingCartList(profileEmail);
-//    }
+
+    // 유저 장바구니 조회
+    @GetMapping("/cart")
+    public ResponseEntity<List<ShoppingCartResponseDTO>> getShoppingCart(){
+        Long userId = TokenContext.getProfileId();
+
+        return cartService.getShoppingCart(userId);
+    }
 
     // 구매 내역 조회
 
--- prj02/src/main/java/com/b2/prj02/service/ProfileService.java
+++ prj02/src/main/java/com/b2/prj02/service/ProfileService.java
@@ -2,8 +2,6 @@ package com.b2.prj02.service;
 
 import com.b2.prj02.dto.response.ProfileInfoResponseDTO;
 
-import com.b2.prj02.dto.response.ShoppingCartListResponseDTO;
-import com.b2.prj02.entity.ShoppingCart;
 import com.b2.prj02.entity.User;
 import com.b2.prj02.repository.ProfileRepository;
 import com.b2.prj02.repository.CartRepository;
